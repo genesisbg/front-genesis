34b73b65023eba9b1ffbea97e7dab22c2062b6a8
SantiRendon <srendon34@soy.sena.edu.co>
2023-06-24 16:13:23 -0500

    middleware que valida token y bcrypt eliminado

--- app/controllers/login.controller.js
+++ app/controllers/login.controller.js
@@ -2,10 +2,10 @@ import jwt from "jsonwebtoken";
 import fetch from "node-fetch";
 
 const authentication = async (req, res) => {
-  const COD_USUARIO = req.body.COD_USUARIO; // 1020402543
-  const CONTRASEÑA = req.body.PASSWORD; // 962012d09b8170d912f0669f6d7d9d07
+  const COD_USUARIO = req.body.COD_USUARIO;
+  const CONTRASENA = req.body.PASSWORD;
 
-  if (COD_USUARIO && CONTRASEÑA) {
+  if (COD_USUARIO && CONTRASENA) {
     try {
 
       const url = `http://localhost:3000/api/user/${COD_USUARIO}`;
@@ -17,7 +17,6 @@ const authentication = async (req, res) => {
       await fetch(url, options)
         .then((response) => response.json())
         .then((datosUsuario) => {
-          // datosUsuario.message ? res.redirect("/login?alert=1") : (payload = datosUsuario[0])
           if (!datosUsuario.message) {
             payload = datosUsuario[0];
           } else {
@@ -26,10 +25,10 @@ const authentication = async (req, res) => {
         });
 
       // creamos una variable token donde mandamos como primer parametro los datos a encriptar y luego la secret key con la cual se desencriptará
-      if (CONTRASEÑA === payload.CONTRASEÑA) {
+      if (CONTRASENA === payload.CONTRASENA) {
         const token = jwt.sign(payload, process.env.SECRET_KEY, {
           expiresIn: process.env.EXPIRE_TOKEN,
-        });
+      });
 
         res.cookie("cookieBG", token);
         return res.redirect("/user/perfil");
@@ -40,7 +39,6 @@ const authentication = async (req, res) => {
       console.error(error);
     }
   } else {
-
     return res.redirect("/login?alert=0");
   }
 };
@@ -54,7 +52,6 @@ const render = async (req, res) => {
       // sin datos en el formulario
       case "0":
         res.render("login", { alertCase: alertCase })
-        // res.render("login")
         break;
 
       // Usuario no existente
@@ -103,7 +100,7 @@ const registro = async (req, res) => {
     "NOM_USUARIO": req.body.NOM_USUARIO,
     "APELL_USUARIO": req.body.APELL_USUARIO,
     "CORREO": req.body.CORREO_USUARIO,
-    "CONTRASEÑA": req.body.PASSWORD,
+    "CONTRASENA": req.body.PASSWORD,
     "FECHA_NAC": req.body.FECHA_NACIMIENTO,
     "SEXO": validarSexo,
     "ESTADO": "ACTIVO",
@@ -111,7 +108,7 @@ const registro = async (req, res) => {
   }
 
   // Valida si nos datos necesarios para el registro si existen
-  if (datosUsuario.DNI_USUARIO && datosUsuario.NOM_USUARIO && datosUsuario.APELL_USUARIO && datosUsuario.CORREO && datosUsuario.CONTRASEÑA && datosUsuario.FECHA_NAC && datosUsuario.SEXO && datosUsuario.ESTADO && datosUsuario.COD_ROL) {
+  if (datosUsuario.DNI_USUARIO && datosUsuario.NOM_USUARIO && datosUsuario.APELL_USUARIO && datosUsuario.CORREO && datosUsuario.CONTRASENA && datosUsuario.FECHA_NAC && datosUsuario.SEXO && datosUsuario.ESTADO && datosUsuario.COD_ROL) {
     try {
       const url = 'http://localhost:3000/api/user';
       const option = {
@@ -135,9 +132,8 @@ const registro = async (req, res) => {
     } catch (error) {
       console.log(error);
     }
-    // res.send(datosUsuario)
   } else {
-    return res.redirect("/login?error=0")
+    return res.redirect("/login?alert=0")
   }
 };
 
