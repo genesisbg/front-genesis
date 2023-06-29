aea4b633f757447ce454e581b25e6381da9baf11
Ayelen <kmlondono14@misena.edu.co>
2023-06-29 17:10:23 -0500

    ..

--- app/controllers/admin.controller.js
+++ app/controllers/admin.controller.js
@@ -330,29 +330,135 @@ const eliminarPrestamos = async (req, res) => {
     })
   return res.redirect("/admin/prestamos");
 }
-const generatePdf = async (req, res, next) => {
-  const html = fs.readFileSync(path.join(__dirname, '../views/template.html'), 'utf-8');
-  const filename = Math.random() + '_doc' + '.pdf';
-  let array = [];
+export const pdf = async (req, res) => {
+  try {
+      const products = await getUser(); // Función para obtener los productos de la API
 
-  const document = {
-      html: html,
-      data: {
-          products: obj
-      },
-      path: './docs/' + filename
+      // Crear un nuevo documento PDF
+      const doc = new PDFDocument();
+
+      // Establecer encabezado
+      doc.font("Helvetica-Bold").fontSize(18).text("Reporte de Usuarios", { align: "center" });
+
+      // Establecer la fecha actual
+      const currentDate = new Date().toLocaleDateString();
+      doc.fontSize(12).text(`Fecha de creación del reporte: ${currentDate}`, { align: "center", margin: [0, 20] });
+
+      // Generar tabla de productos
+      generatePDFTable(doc, user);
+
+      // Establecer el nombre del archivo y el tipo de contenido de la respuesta
+      res.setHeader("Content-Disposition", "attachment; filename=reporte_usuarios.pdf");
+      res.setHeader("Content-Type", "application/pdf");
+
+      // Envía el documento PDF como respuesta
+      doc.pipe(res);
+      doc.end();
+  } catch (error) {
+      res.status(500).send(error.message);
+      console.log(error.message);
   }
-  pdf.create(document, options)
-      .then(res => {
-          console.log(res);
-      }).catch(error => {
-          console.log(error);
-      });
+};
+const generatePDFTable = (doc, products) => {
+  const rolText = {
+    1: 'Usuario',
+    2: 'Administrador'
 
-      res.render('download', {
-          path: filepath
+};
+
+  const tableHeaders = ["ID", "Nombre", "Apellido", "estado", "Rol"];
+
+  // Establecer posición inicial de la tabla
+  let y = doc.y + 30;
+
+  // Establecer estilos para los encabezados de la tabla
+  doc.font("Helvetica-Bold").fontSize(10);
+  doc.fillColor("black");
+
+  // Dibujar los encabezados de la tabla
+  tableHeaders.forEach((header, columnIndex) => {
+      doc.text(header, columnIndex * 100 + 50, y);
+  });
+
+  // Establecer estilos para las filas de la tabla
+  doc.font("Helvetica").fontSize(10);
+
+  // Dibujar las filas de la tabla
+  products.forEach((user, rowIndex) => {
+      y += 20; // Aumentar la posición vertical para cada fila
+
+      const rowData = [
+          user.COD_USUARIO,
+          user.NOM_USUARIO,
+          user.APELLIDO,
+          user.ESTADO,
+          rolText[user.ROL],
+  
+      ];
+
+      rowData.forEach((data, columnIndex) => {
+          const cellWidth = 100;
+          const cellHeight = 20;
+
+          const textOptions = {
+              width: cellWidth,
+              height: cellHeight,
+              lineBreak: false
+          };
+
+          doc.text(data, columnIndex * 100 + 50, y, textOptions);
       });
-}
+  });
+};
+/**
+ * Genera un reporte en formato Excel de los productos obtenidos de la API.
+ * El reporte se genera en un archivo de Excel y se descarga como adjunto en la respuesta HTTP.
+ */
+ export const excel = async (req, res) => {
+  try {
+      const products = await getUser(); // Función para obtener los productos de la API
+
+      // Crear una nueva hoja de cálculo
+      const workbook = XLSX.utils.book_new();
+
+      // Crear una nueva hoja dentro del libro de Excel
+      const worksheet = XLSX.utils.json_to_sheet(user);
+
+      // Agregar la hoja al libro de Excel
+      XLSX.utils.book_append_sheet(workbook, worksheet, "Usuarios");
+
+      // Convertir el libro de Excel a un archivo de buffer
+      const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "buffer" });
+
+      // Establecer el nombre del archivo y el tipo de contenido de la respuesta
+      res.setHeader("Content-Disposition", "attachment; filename=reporte_usuarios.xlsx");
+      res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+
+      // Envía el archivo de Excel como respuesta
+      res.send(excelBuffer);
+  } catch (error) {
+      res.status(500).send(error.message);
+      console.log(error.message);
+  }
+};
+
+// Donde tiene las rutas llama a los controladores 
+// router.get('/pdf', dash.pdf);
+// router.get('/excel', dash.excel);
+// Botones que invocan la ruta para generar el 
+                              // <button id="exportPdfButton" onclick="window.location.href = '/dash/pdf'">Generar reporte PDF</button>
+                              // <button id="exportExcelButton" onclick="window.location.href = '/dash/excel'">Generar reporte Excel</button>
+
+// Constante para llamar a la base de datos se pone en el mismo archivo
+// Donde 
+const getUser = async () => {
+  const response = await fetch("http://localhost:3000/api/user");
+  const user = await response.json();
+  console.log(user);
+  return user;
+};
+
+
 export const adminController = {
     dash,
     agregarLibros,
@@ -366,5 +472,6 @@ export const adminController = {
     eliminarPrestamos,
     insertarLibros,
     editarLibros,
-    generatePdf
+    pdf,
+    excel
 }
